c45c1be52f548cc77c7714dbd75aca0594eeedfc
Natã Pedro <natanpedrodasilva@gmail.com>
2025-10-03 01:08:08 -0300

    chore(structure): Adiciona arquivos de inicialização do Spring Boot, Vite e configurações do ambiente.

---
+++ transfer-scheduling-be/src/main/java/com/tokiomarine/schedule/transfer/TransferSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.schedule.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferSchedulingApplication.class, args);
+	}
+
+}
