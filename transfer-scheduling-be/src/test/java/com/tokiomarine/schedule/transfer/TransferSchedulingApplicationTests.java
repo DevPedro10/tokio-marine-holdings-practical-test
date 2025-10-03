40038e7a3313816256f40d57b691aa8b96430e1c
Natã Pedro <natanpedrodasilva@gmail.com>
2025-10-03 01:10:17 -0300

    docs(project): Adiciona documentação (README) e ignora arquivos de build do Frontend.

---
+++ transfer-scheduling-be/src/test/java/com/tokiomarine/schedule/transfer/TransferSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.schedule.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
